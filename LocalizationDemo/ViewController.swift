73ec2b7acf0b018cd461e8d4732487d20f381cf0
aliyun4827826669 <userlianjie@gmail.com>
2024-01-04 11:54:52 +0800

    Initial project commit

---
+++ LocalizationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalizationDemo
+//
+//  Created by 李剑杰 on 1/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
